fae64671a75953b8fc2bb5247aedd96537e2570e
jingyu <jingyu_722@163.com>
2021-12-02 18:56:02 +0800

    暂停..

---
+++ flaskr/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # 看一下这个装饰器的代码，如何实现装饰器传参的
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
